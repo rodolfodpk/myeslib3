cf760cad33b5427672c78339e0b7faf245bf3642
Rodolfo <rodolfo.paula@locaweb.com.br>
2017-03-15 00:40:39 -0300

    SnapshotRead as FunctionalInterface

--- myeslib3-stack/src/main/java/myeslib3/stack/persistence/SnapshotReader.java
+++ myeslib3-stack/src/main/java/myeslib3/stack/persistence/SnapshotReader.java
@@ -2,24 +2,20 @@ package myeslib3.stack.persistence;
 
 import lombok.Value;
 import myeslib3.core.data.AggregateRoot;
-import myeslib3.core.data.Event;
-import myeslib3.core.data.UnitOfWork;
 import myeslib3.core.data.Version;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+@FunctionalInterface
 public interface SnapshotReader<A extends AggregateRoot> {
 
   Snapshot<A> getSnapshot(final String id);
-
-  default List<Event> flatMap(final List<UnitOfWork> unitOfWorks) {
-    return unitOfWorks.stream().flatMap((unitOfWork) -> unitOfWork.getEvents().stream()).collect(Collectors.toList());
-  }
-
-  default Version lastVersion(final List<UnitOfWork> unitOfWorks) {
-    return unitOfWorks.isEmpty() ? Version.create(0) : unitOfWorks.get(unitOfWorks.size()-1).getVersion();
-  }
+//
+//  default List<Event> flatMap(final List<UnitOfWork> unitOfWorks) {
+//    return unitOfWorks.stream().flatMap((unitOfWork) -> unitOfWork.getEvents().stream()).collect(Collectors.toList());
+//  }
+//
+//  default Version lastVersion(final List<UnitOfWork> unitOfWorks) {
+//    return unitOfWorks.isEmpty() ? Version.create(0) : unitOfWorks.get(unitOfWorks.size()-1).getVersion();
+//  }
 
   @Value
   class Snapshot<A> {
